b26a613aa6d28215c22d7fedee8d973a81c5a3cb
almhh <56926323+almhh@users.noreply.github.com>
2020-04-21 16:33:02 +0200

    Update style and add doubling time lines

--- index.js
+++ index.js
@@ -1,68 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-      const options = {
-        title: {
-          text: 'Döda per region, 7 dagars rullande medelvärde (log)'
+  const options = {
+    title: {
+      text: 'Dagliga dödsfall per region, 7 dagars rullande medelvärde (log-skala)'
+    },
+    credits: {
+      enabled: false
+    },
+    yAxis: {
+      title: {
+        text: ''
+      },
+      type: 'logarithmic',
+      allowDecimals: false,
+      min: 1,
+      max: 100
+    },
+    xAxis: {
+      title: {
+        text: 'Dagar sedan dagliga dödsfall passerat 3 →'
+      },
+      type: 'linear',
+      allowDecimals: false
+    },
+    tooltip: {
+            pointFormat: '{series.name}: <b>{point.y:.0f}</b><br/>',
+            shared: false
         },
-        credits: {
+    plotOptions: {
+      series: {
+        marker: {
           enabled: false
-        },
-        yAxis: {
-          title: {
-            text: ''
-          },
-          type: 'logarithmic',
-          allowDecimals: false,
-          min: 1,
-          max: 100
-        },
-        xAxis: {
-          title: {
-            text: 'Dagar sedan dagliga dödsfall passerat 3 →'
-          },
-          type: 'linear',
-          allowDecimals: false
         }
-      };
+      }
+    },
+    series: [
+      {color: "#000", lineWidth: 1, dashStyle: 'dot', showInLegend: false},
+      {color: "#000", lineWidth: 1, dashStyle: 'dot', showInLegend: false},
+      {color: "#000", lineWidth: 1, dashStyle: 'dot', showInLegend: false}]
+  };
 
-      const optionsLin = {
-        title: {
-          text: 'Döda per region, 7 dagars rullande medelvärde (linjär)'
-        },
-        credits: {
-          enabled: false
-        },
-        yAxis: {
-          title: {
-            text: ''
-          },
-          type: 'linear',
-          allowDecimals: false,
-          min: 0,
-          max: 100,
-          tickAmount: 5
-        },
-        xAxis: {
-          title: {
-            text: 'Dagar sedan dagliga dödsfall passerat 3 →'
-          },
-          type: 'linear',
-          allowDecimals: false
-        }
-      };
+  $.get('test.csv', csv => {
+    options.data = {
+      csv
+    };
 
-      $.get('test.csv', csv => {
-        options.data = {
-          csv
-        };
-          Highcharts.chart('container-log', options);
-        });
+    Highcharts.chart('container-log', options);
 
-        $.get('test.csv', csv => {
-          optionsLin.data = {
-            csv
-          };
-            Highcharts.chart('container-lin', optionsLin);
-          });
+  });
 
-      });
+});
